1228464763ac33fa11fde37af410164968fcdf8a
wyatt <wyatth@gmail.com>
2016-02-23 11:47:20 +0800

    revise unit test

--- Utility_test.go
+++ Utility_test.go
@@ -5,21 +5,62 @@ import (
     "strconv"
 )
 
-func Test_relativeToAbsoluteTime(t *testing.T) {
-    input := "5"
+func Test_relativeToAbsoluteTimeInDay(t *testing.T) {
+    input := "256"
     now := time.Now()
-    ret, err := relativeToAbsoluteTime(now, input, "M")
+
+    ret, err := relativeToAbsoluteTime(now, input, "d")
     if err != nil {
-        t.Error("relativeToAbsoluteTime err")    
+        t.Error(err)    
     } else {
         diff, err := strconv.ParseInt(input, 10, 64)
         if err != nil {
-            t.Error("relativeToAbsoluteTime parse int err")   
+            t.Error( err)   
+        }
+        tResult := now.AddDate(0, 0,int(-diff))
+
+        if tResult.UTC().Unix() * 1000 != ret {
+            t.Error(ret)   
+        }
+    }
+}
+func Test_relativeToAbsoluteTimeInMonth(t *testing.T) {
+    input := "5"
+    now := time.Now()
+
+    ret, err := relativeToAbsoluteTime(now, input, "M")
+    if err != nil {
+        t.Error(err)    
+    } else {
+        diff, err := strconv.ParseInt(input, 10, 64)
+        if err != nil {
+            t.Error( err)   
         }
         tResult := now.AddDate(0, int(-diff), 0)
 
         if tResult.UTC().Unix() * 1000 != ret {
-            t.Error("relativeToAbsoluteTime result err")   
+            t.Error(ret)   
+        }
+    }
+}
+
+func Test_relativeToAbsoluteTimeInYear(t *testing.T) {
+    input := "5"
+    now := time.Now()
+
+    ret, err := relativeToAbsoluteTime(now, input, "y")
+    if err != nil {
+        t.Error(err)    
+    } else {
+        diff, err := strconv.ParseInt(input, 10, 64)
+        if err != nil {
+            t.Error(err)   
+        }
+        tResult := now.AddDate(int(-diff), 0, 0)
+
+        if tResult.UTC().Unix() * 1000 != ret {
+            t.Error(ret)   
+            t.Error(tResult.UTC().Unix() * 1000 )   
         }
     }
 }
