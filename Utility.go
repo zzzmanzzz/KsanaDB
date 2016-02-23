2b96517fe78c10e84710dd61dfebcf9c4bfddcd1
wyatt <wyatth@gmail.com>
2016-02-23 13:51:45 +0800

    add sub day tool

--- Utility.go
+++ Utility.go
@@ -12,31 +12,31 @@ func getDateStartSec(timestamp int64) ( int64, int64 ) {
         DateStart :=  tm.Format(shortForm)
         st, _ := time.Parse(shortForm, DateStart) 
         dateZeroOclock := st.UTC().Unix() * 1000 
-        //fmt.Println(st.UTC().Unix()) 
-        //fmt.Println(timestamp) 
-        //fmt.Println(timestamp - 1000 * st.UTC().Unix() ) 
         return dateZeroOclock, timestamp - dateZeroOclock 
 } 
 
-func relativeToAbsoluteTime(tNow time.Time, input string, unit string) (int64, error) {
+func relativeToAbsoluteTime(tNow time.Time, diff int, unit string) (int64, error) {
     var tResult time.Time 
-    diff, err := strconv.ParseInt(input, 10, 64)
-
-    if err == nil {
-        if unit == "ms" {
-        } else if unit == "s" {
-        } else if unit == "m" {
-        } else if unit == "h" {
-        } else if unit == "d" {
-            tResult = tNow.AddDate(0, 0, int(-diff))
-        } else if unit == "w" {
-            tResult = tNow.AddDate(0, 0, 7 * int(-diff))
-        } else if unit == "M" {
-            tResult = tNow.AddDate(0, int(-diff), 0)
-        } else if unit == "y" {
-            tResult = tNow.AddDate(int(-diff), 0, 0)
-        }
+    if unit == "ms" || unit == "s" || unit == "m" || unit == "h" {
+         d := strconv.Itoa(-diff)           
+         dur, err := time.ParseDuration(d + unit)
+         if err == nil {
+         tResult = tNow.Add(dur)
+             return tResult.UTC().Unix() * 1000, err
+         } else {
+             return 0, err    
+         }
+    } 
+        
+    if unit == "d" {
+        tResult = tNow.AddDate(0, 0, -diff)
+    } else if unit == "w" {
+        tResult = tNow.AddDate(0, 0, 7 * -diff)
+    } else if unit == "M" {
+        tResult = tNow.AddDate(0, -diff, 0)
+    } else if unit == "y" {
+        tResult = tNow.AddDate(-diff, 0, 0)
     }
-    return tResult.UTC().Unix() * 1000, err
+    return tResult.UTC().Unix() * 1000, nil 
 }
 
--- Utility_test.go
+++ Utility_test.go
@@ -5,18 +5,87 @@ import (
     "strconv"
 )
 
+func Test_relativeToAbsoluteTimeInMilliSecond(t *testing.T) {
+    input := 256
+    now := time.Now()
+
+    ret, err := relativeToAbsoluteTime(now, input, "ms")
+    if err != nil {
+        t.Error(err)    
+    } else {
+        diff := input
+        d := strconv.Itoa(-diff)
+        dur,_  := time.ParseDuration(d + "ms")
+        tResult := now.Add(dur) 
+        if tResult.UTC().Unix() * 1000 != ret {
+            t.Error(ret)    
+        }
+    }
+}
+
+func Test_relativeToAbsoluteTimeInSecond(t *testing.T) {
+    input := 256
+    now := time.Now()
+
+    ret, err := relativeToAbsoluteTime(now, input, "s")
+    if err != nil {
+        t.Error(err)    
+    } else {
+        diff := input
+        d := strconv.Itoa(-diff)
+        dur,_  := time.ParseDuration(d + "s")
+        tResult := now.Add(dur) 
+        if tResult.UTC().Unix() * 1000 != ret {
+            t.Error(ret)    
+        }
+    }
+}
+
+func Test_relativeToAbsoluteTimeInMinute(t *testing.T) {
+    input := 256
+    now := time.Now()
+
+    ret, err := relativeToAbsoluteTime(now, input, "m")
+    if err != nil {
+        t.Error(err)    
+    } else {
+        diff := input
+        d := strconv.Itoa(-diff)
+        dur,_  := time.ParseDuration(d + "m")
+        tResult := now.Add(dur) 
+        if tResult.UTC().Unix() * 1000 != ret {
+            t.Error(ret)    
+        }
+    }
+}
+
+func Test_relativeToAbsoluteTimeInHour(t *testing.T) {
+    input := 256
+    now := time.Now()
+
+    ret, err := relativeToAbsoluteTime(now, input, "h")
+    if err != nil {
+        t.Error(err)    
+    } else {
+        diff := input
+        d := strconv.Itoa(-diff)
+        dur,_  := time.ParseDuration(d + "h")
+        tResult := now.Add(dur) 
+        if tResult.UTC().Unix() * 1000 != ret {
+            t.Error(ret)    
+        }
+    }
+}
+
 func Test_relativeToAbsoluteTimeInDay(t *testing.T) {
-    input := "256"
+    input := 256
     now := time.Now()
 
     ret, err := relativeToAbsoluteTime(now, input, "d")
     if err != nil {
         t.Error(err)    
     } else {
-        diff, err := strconv.ParseInt(input, 10, 64)
-        if err != nil {
-            t.Error( err)   
-        }
+        diff := input
         tResult := now.AddDate(0, 0,int(-diff))
 
         if tResult.UTC().Unix() * 1000 != ret {
@@ -24,18 +93,16 @@ func Test_relativeToAbsoluteTimeInDay(t *testing.T) {
         }
     }
 }
+
 func Test_relativeToAbsoluteTimeInMonth(t *testing.T) {
-    input := "5"
+    input := 5
     now := time.Now()
 
     ret, err := relativeToAbsoluteTime(now, input, "M")
     if err != nil {
         t.Error(err)    
     } else {
-        diff, err := strconv.ParseInt(input, 10, 64)
-        if err != nil {
-            t.Error( err)   
-        }
+        diff := input
         tResult := now.AddDate(0, int(-diff), 0)
 
         if tResult.UTC().Unix() * 1000 != ret {
@@ -45,22 +112,18 @@ func Test_relativeToAbsoluteTimeInMonth(t *testing.T) {
 }
 
 func Test_relativeToAbsoluteTimeInYear(t *testing.T) {
-    input := "5"
+    input := 5
     now := time.Now()
 
     ret, err := relativeToAbsoluteTime(now, input, "y")
     if err != nil {
         t.Error(err)    
     } else {
-        diff, err := strconv.ParseInt(input, 10, 64)
-        if err != nil {
-            t.Error(err)   
-        }
+        diff := input
         tResult := now.AddDate(int(-diff), 0, 0)
 
         if tResult.UTC().Unix() * 1000 != ret {
             t.Error(ret)   
-            t.Error(tResult.UTC().Unix() * 1000 )   
         }
     }
 }
